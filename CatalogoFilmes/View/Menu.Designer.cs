a93305bcd60e786203b1a98532c60f40a235e670
silvioflorentino <professorsilvioflorentino@yahoo.com.br>
2023-06-27 11:26:23 -0300

    telas da serie

---
+++ CatalogoFilmes/View/Menu.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace CatalogoFilmes.View
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(78, 82);
+            button1.Name = "button1";
+            button1.Size = new Size(234, 110);
+            button1.TabIndex = 0;
+            button1.Text = "Cadastrar Filme";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(78, 229);
+            button2.Name = "button2";
+            button2.Size = new Size(234, 126);
+            button2.TabIndex = 1;
+            button2.Text = "Cadastrar Serie";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // Menu
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(398, 450);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            MaximizeBox = false;
+            Name = "Menu";
+            ShowIcon = false;
+            Text = "Tela Principal";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+    }
+}
